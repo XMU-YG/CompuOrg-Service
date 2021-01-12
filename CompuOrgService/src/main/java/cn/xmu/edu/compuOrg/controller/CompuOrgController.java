9f71257d30c814f5bb170b6082bf3e4cc00b532a
Snow02203835 <snow02203835@163.com>
2021-01-12 23:07:32 +0800

    新增接口

--- CompuOrgService/src/main/java/cn/xmu/edu/compuOrg/controller/CompuOrgController.java
+++ CompuOrgService/src/main/java/cn/xmu/edu/compuOrg/controller/CompuOrgController.java
@@ -1,5 +1,6 @@
 package cn.xmu.edu.compuOrg.controller;
 
+import cn.xmu.edu.Core.util.Common;
 import cn.xmu.edu.Core.util.ResponseCode;
 import cn.xmu.edu.Core.util.ReturnObject;
 import cn.xmu.edu.compuOrg.model.vo.LinesVo;
@@ -11,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletResponse;
+import java.util.List;
 
 @Api(value = "计组项目后端", tags = "compuOrg")
 @RestController /*Restful的Controller对象*/
@@ -40,19 +42,39 @@ public class CompuOrgController {
     })
     @ApiResponses({
             @ApiResponse(code = 0, message = "成功"),
-            @ApiResponse(code = 601, message = "连线端点不存在"),
+            @ApiResponse(code = 601, message = "本次实验中不存在这样的连线"),
     })
-    @PostMapping("experiment/{eid}/lines")
+    @PostMapping("experiment/{eid}/lines/connect")
     public Object connectLines(@PathVariable(value = "eid") Integer eid, @RequestBody LinesVo linesVo){
         logger.debug("experimentId: " + eid + ", lines: " + linesVo.toString());
         if(eid < 1 || eid > 5){
-            return new ReturnObject(ResponseCode.RESOURCE_ID_NOTEXIST);
+            return Common.decorateReturnObject(new ReturnObject(ResponseCode.RESOURCE_ID_NOTEXIST));
         }
         if(linesVo.getEndA() == null || linesVo.getEndB() == null){
-            return new ReturnObject(ResponseCode.FIELD_NOTVALID);
+            return Common.decorateReturnObject(new ReturnObject(ResponseCode.FIELD_NOTVALID));
         }
-        return compuOrgService.connectLines(eid, linesVo);
+        return Common.decorateReturnObject(compuOrgService.connectLines(eid, linesVo));
     }
 
+
+    @ApiOperation(value = "连线检验", produces = "application/json")
+    @ApiImplicitParams({
+            @ApiImplicitParam(paramType = "header", dataType = "String", name = "authorization", value = "token", required = false),
+            @ApiImplicitParam(paramType = "path", dataType = "int", name = "eid", value = "实验序号", required = true),
+            @ApiImplicitParam(paramType = "body", dataType = "LinesVo", name = "linesVos", allowMultiple = true, value = "连线", required = true),
+
+    })
+    @ApiResponses({
+            @ApiResponse(code = 0, message = "成功"),
+            @ApiResponse(code = 602, message = "存在错误布线"),
+    })
+    @PostMapping("experiment/{eid}/lines/validation")
+    public Object validLines(@PathVariable(value = "eid") Integer eid, @RequestBody List<LinesVo> linesVos){
+        logger.debug("experimentId: " + eid + ", lines: " + linesVos.toString());
+        if(eid < 1 || eid > 5){
+            return Common.decorateReturnObject(new ReturnObject(ResponseCode.RESOURCE_ID_NOTEXIST));
+        }
+        return Common.decorateReturnObject(compuOrgService.validAllLines(eid, linesVos));
+    }
 
 }
--- CompuOrgService/src/main/java/cn/xmu/edu/compuOrg/service/CompuOrgService.java
+++ CompuOrgService/src/main/java/cn/xmu/edu/compuOrg/service/CompuOrgService.java
@@ -1,7 +1,41 @@
 package cn.xmu.edu.compuOrg.service;
 
+import cn.xmu.edu.Core.util.ResponseCode;
+import cn.xmu.edu.compuOrg.dao.ExperimentLinesDao;
+import cn.xmu.edu.Core.util.ReturnObject;
+import cn.xmu.edu.compuOrg.model.vo.LinesVo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class CompuOrgService {
+
+    @Autowired
+    private ExperimentLinesDao experimentLinesDao;
+
+    /**
+     * @author snow create 2021/01/12 20:26
+     * @param experimentId
+     * @param linesVo
+     * @return
+     */
+    public ReturnObject connectLines(Integer experimentId, LinesVo linesVo){
+        if(experimentLinesDao.validSingleConnection(experimentId, linesVo)){
+            return new ReturnObject(ResponseCode.OK);
+        }
+        else{
+            return new ReturnObject(ResponseCode.LINE_ENDS_NOT_VALID);
+        }
+    }
+
+    public ReturnObject validAllLines(Integer experimentId, List<LinesVo> linesVos){
+        if(experimentLinesDao.validAllLines(experimentId, linesVos)){
+            return new ReturnObject(ResponseCode.OK);
+        }
+        else {
+            return new ReturnObject(ResponseCode.LINE_CONNECT_ERROR);
+        }
+    }
 }
