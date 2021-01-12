e8967c5543c158694523f570a93424516527d2d7
Snow02203835 <snow02203835@163.com>
2021-01-12 20:16:08 +0800

    删除尚未用到的注解

--- CompuOrgService/src/main/java/cn/xmu/edu/compuOrg/CompuOrgServiceApplication.java
+++ CompuOrgService/src/main/java/cn/xmu/edu/compuOrg/CompuOrgServiceApplication.java
@@ -1,14 +1,9 @@
 package cn.xmu.edu.compuOrg;
 
-import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@EnableDubbo
-@EnableDiscoveryClient
-@SpringBootApplication(scanBasePackages = {"cn.edu.xmu.compuOrg"})
+@SpringBootApplication(scanBasePackages = {"cn.xmu.edu.compuOrg"})
 //@MapperScan("cn.edu.xmu.compuOrg.mapper")
 public class CompuOrgServiceApplication {
     public static void main(String[] args) {
