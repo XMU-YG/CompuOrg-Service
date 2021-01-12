af1c5aba7b8a7b8ef6722c2762a2c12d61333ad8
Snow02203835 <snow02203835@163.com>
2021-01-12 21:04:20 +0800

    新增controller层的连线接口

--- CompuOrgService/src/main/java/cn/xmu/edu/compuOrg/controller/CompuOrgController.java
+++ CompuOrgService/src/main/java/cn/xmu/edu/compuOrg/controller/CompuOrgController.java
@@ -1,11 +1,14 @@
 package cn.xmu.edu.compuOrg.controller;
 
+import cn.xmu.edu.Core.util.ResponseCode;
+import cn.xmu.edu.Core.util.ReturnObject;
+import cn.xmu.edu.compuOrg.model.vo.LinesVo;
 import cn.xmu.edu.compuOrg.service.CompuOrgService;
 import io.swagger.annotations.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletResponse;
 
@@ -14,6 +17,8 @@ import javax.servlet.http.HttpServletResponse;
 @RequestMapping(value = "/", produces = "application/json;charset=UTF-8")
 public class CompuOrgController {
 
+    private  static  final Logger logger = LoggerFactory.getLogger(CompuOrgController.class);
+
     @Autowired
     private CompuOrgService compuOrgService;
 
@@ -21,19 +26,32 @@ public class CompuOrgController {
     private HttpServletResponse httpServletResponse;
 
     /**
-     * @author snow
+     * @author snow 2021/01/12 20:23
+     * @param eid
+     * @param linesVo
+     * @return
      */
-    @ApiOperation(value = "测试", produces = "application/json")
+    @ApiOperation(value = "实验连线", produces = "application/json")
     @ApiImplicitParams({
-            @ApiImplicitParam(paramType = "header", dataType = "String", name = "authorization", value = "token", required = true),
+            @ApiImplicitParam(paramType = "header", dataType = "String", name = "authorization", value = "token", required = false),
+            @ApiImplicitParam(paramType = "path", dataType = "int", name = "eid", value = "实验序号", required = true),
+            @ApiImplicitParam(paramType = "body", dataType = "LinesVo", name = "linesVo", value = "连线端点", required = true),
+
     })
     @ApiResponses({
             @ApiResponse(code = 0, message = "成功"),
-            @ApiResponse(code = 801, message = "订单状态禁止"),
+            @ApiResponse(code = 601, message = "连线端点不存在"),
     })
-    @PutMapping("test")
-    public Object confirmOrderReceived( ){
-        return "{message: \"Hello World!\"}";
+    @PostMapping("experiment/{eid}/lines")
+    public Object connectLines(@PathVariable(value = "eid") Integer eid, @RequestBody LinesVo linesVo){
+        logger.debug("experimentId: " + eid + ", lines: " + linesVo.toString());
+        if(eid < 1 || eid > 5){
+            return new ReturnObject(ResponseCode.RESOURCE_ID_NOTEXIST);
+        }
+        if(linesVo.getEndA() == null || linesVo.getEndB() == null){
+            return new ReturnObject(ResponseCode.FIELD_NOTVALID);
+        }
+        return compuOrgService.connectLines(eid, linesVo);
     }
 
 
