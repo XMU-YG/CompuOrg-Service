6f80fb55cef3d24daf62d573718b10b6a80ada03
Snow02203835 <snow02203835@163.com>
2021-01-18 13:45:15 +0800

    新增 连线列表Vo

--- CompuOrgService/src/main/java/cn/xmu/edu/compuOrg/model/vo/LinesVo.java
+++ CompuOrgService/src/main/java/cn/xmu/edu/compuOrg/model/vo/LinesVo.java
@@ -3,12 +3,15 @@ package cn.xmu.edu.compuOrg.model.vo;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+import java.util.List;
+
 @Data
 public class LinesVo {
 
-    @ApiModelProperty(value = "连线A端")
-    private String endA;
-
-    @ApiModelProperty(value = "连线B端")
-    private String endB;
+    @ApiModelProperty(value = "连线列表")
+    @NotNull
+    @Valid
+    private List<LineVo> lines;
 }
