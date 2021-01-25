0cf7bc7f27261abd9943629361b33cbd86cf03cf
Snow02203835 <snow02203835@163.com>
2021-01-25 22:35:22 +0800

    新增 TestVo.java

---
+++ CompuOrgService/src/main/java/cn/xmu/edu/compuOrg/model/vo/TestVo.java
@@ -0,0 +1,13 @@
+package cn.xmu.edu.compuOrg.model.vo;
+
+import lombok.Data;
+
+import java.util.List;
+
+/**
+ * @author snow create 2021/01/25 22:05
+ */
+@Data
+public class TestVo {
+    private List<TopicAnswerVo> topicAnswerVos;
+}
