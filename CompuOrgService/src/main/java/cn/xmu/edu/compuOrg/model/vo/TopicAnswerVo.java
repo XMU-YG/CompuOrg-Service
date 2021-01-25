97d762a101d376e9fa8b72954dd9e3d342defa5b
Snow02203835 <snow02203835@163.com>
2021-01-25 22:34:59 +0800

    新增 TopicAnswerVo.java

---
+++ CompuOrgService/src/main/java/cn/xmu/edu/compuOrg/model/vo/TopicAnswerVo.java
@@ -0,0 +1,12 @@
+package cn.xmu.edu.compuOrg.model.vo;
+
+import lombok.Data;
+
+/**
+ * @author snow create 2021/01/25 22:04
+ */
+@Data
+public class TopicAnswerVo {
+    private Long topicId;
+    private String answer;
+}
