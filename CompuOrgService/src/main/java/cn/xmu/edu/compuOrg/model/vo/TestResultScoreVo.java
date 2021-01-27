0be8918cfb8123ab3a65ca1f9fe5b13e2a90e367
Snow02203835 <snow02203835@163.com>
2021-01-27 23:20:07 +0800

    新增 TestResultScoreVo.java

---
+++ CompuOrgService/src/main/java/cn/xmu/edu/compuOrg/model/vo/TestResultScoreVo.java
@@ -0,0 +1,22 @@
+package cn.xmu.edu.compuOrg.model.vo;
+
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+import java.util.List;
+
+/**
+ * @author snow create 2021/01/27 22:09
+ */
+@Data
+public class TestResultScoreVo {
+    @ApiModelProperty(value = "测试结果id")
+    @NotNull
+    private Long testResultId;
+
+    @ApiModelProperty(value = "各题目答案得分")
+    @Valid
+    private List<TopicAnswerScoreVo> topicAnswerScores;
+}
