2ea348f82b6f66bc1f665d06f728decc1f1d03e1
Snow02203835 <snow02203835@163.com>
2021-01-28 14:13:51 +0800

    新增 experimentId属性

--- CompuOrgService/src/main/java/cn/xmu/edu/compuOrg/model/vo/TestVo.java
+++ CompuOrgService/src/main/java/cn/xmu/edu/compuOrg/model/vo/TestVo.java
@@ -2,6 +2,9 @@ package cn.xmu.edu.compuOrg.model.vo;
 
 import lombok.Data;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
 import java.util.List;
 
 /**
@@ -9,5 +12,9 @@ import java.util.List;
  */
 @Data
 public class TestVo {
+    @Min(value = 1, message = "实验序号不能小于1")
+    @Max(value = 5, message = "实验序号不能大于5")
+    @NotNull(message = "实验序号不能为空")
+    private Long experimentId;
     private List<TopicAnswerVo> topicAnswerVos;
 }
