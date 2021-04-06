81470af055556ee428239742f2410ccca3b8b6f0
Snow02203835 <snow02203835@163.com>
2021-04-06 17:28:01 +0800

    新增 传入参数为空白字符时返回字段不合法

--- CompuOrgService/src/main/java/cn/xmu/edu/compuOrg/model/vo/VerifyCodeVo.java
+++ CompuOrgService/src/main/java/cn/xmu/edu/compuOrg/model/vo/VerifyCodeVo.java
@@ -3,12 +3,12 @@ package cn.xmu.edu.compuOrg.model.vo;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
-import javax.validation.constraints.NotNull;
+import javax.validation.constraints.NotBlank;
 
 @Data
 public class VerifyCodeVo {
 
     @ApiModelProperty(value = "验证码")
-    @NotNull
+    @NotBlank(message = "验证码不能为空")
     private String verifyCode;
 }
