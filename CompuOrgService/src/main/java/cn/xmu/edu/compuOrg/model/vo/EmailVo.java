4ee0335f9118398be9ee62c829332e74f6c775ba
Snow02203835 <snow02203835@163.com>
2021-04-06 17:25:20 +0800

    新增 邮箱为空白字符返回不合法

--- CompuOrgService/src/main/java/cn/xmu/edu/compuOrg/model/vo/EmailVo.java
+++ CompuOrgService/src/main/java/cn/xmu/edu/compuOrg/model/vo/EmailVo.java
@@ -4,6 +4,7 @@ import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
 /**
@@ -14,6 +15,7 @@ import javax.validation.constraints.NotNull;
 public class EmailVo {
 
     @ApiModelProperty(value = "邮箱")
+    @NotBlank(message = "邮箱不能为空")
     @NotNull(message = "邮箱不能为空")
     private String email;
 }
