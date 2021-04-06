ff66b71f6ee57293a1410b5d142f22b75e9f5d0a
Snow02203835 <snow02203835@163.com>
2021-04-06 17:27:24 +0800

    新增 传入参数为空白字符时返回字段不合法

--- CompuOrgService/src/main/java/cn/xmu/edu/compuOrg/model/vo/UserVo.java
+++ CompuOrgService/src/main/java/cn/xmu/edu/compuOrg/model/vo/UserVo.java
@@ -3,6 +3,7 @@ package cn.xmu.edu.compuOrg.model.vo;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
 @Data
@@ -10,14 +11,17 @@ public class UserVo {
 
     @ApiModelProperty(value = "用户名")
     @NotNull(message = "用户名不能为空")
+    @NotBlank(message = "用户名不能为空")
     private String userName;
 
     @ApiModelProperty(value = "密码")
     @NotNull(message = "密码不能为空")
+    @NotBlank(message = "密码不能为空")
     private String password;
 
     @ApiModelProperty(value = "验证码")
     @NotNull(message = "验证码不能为空")
+    @NotBlank(message = "验证码不能为空")
     private String verifyCode;
 
     @ApiModelProperty(value = "真实姓名")
@@ -28,6 +32,7 @@ public class UserVo {
 
     @ApiModelProperty(value = "邮箱")
     @NotNull(message = "邮箱不能为空")
+    @NotBlank(message = "邮箱不能为空")
     private String email;
 
     @ApiModelProperty(value = "电话号码")
