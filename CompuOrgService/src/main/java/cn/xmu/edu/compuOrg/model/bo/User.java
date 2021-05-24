6bcd1ba3de8a01ee3e08923bc99d82d29b13dce1
Snow02203835 <snow02203835@163.com>
2021-05-24 10:55:37 +0800

    修改 gender赋值方式

--- CompuOrgService/src/main/java/cn/xmu/edu/compuOrg/model/bo/User.java
+++ CompuOrgService/src/main/java/cn/xmu/edu/compuOrg/model/bo/User.java
@@ -87,15 +87,6 @@ public class User implements VoObject, Serializable {
         return null;
     }
 
-    public void setGender(String gender){
-        if(gender.equals("男")){
-            this.gender = (byte)1;
-        }
-        else if(gender.equals("女")){
-            this.gender = (byte)0;
-        }
-    }
-
     /**
      * 通过userBasicInfo中非空的属性更新属性值
      * @author snow create 2021/01/23 13:53
@@ -109,7 +100,7 @@ public class User implements VoObject, Serializable {
             this.mobile = AES.encrypt(userBasicInfoVo.getMobile(), AES_PASS);
         }
         if(userBasicInfoVo.getGender() != null){
-            setGender(userBasicInfoVo.getGender());
+            this.gender = userBasicInfoVo.getGender();
         }
         if(userBasicInfoVo.getRealName() != null){
             this.realName = userBasicInfoVo.getRealName();
